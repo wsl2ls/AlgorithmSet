62291415dfd0b19892193e0514837bd3409e412d
wsl <15324956576@163.com>
2019-03-12 18:05:09 +0800

    消息转发流程
    消息转发流程

--- AlgorithmSet/ViewController.m
+++ AlgorithmSet/ViewController.m
@@ -10,6 +10,15 @@
 #import "SLTableViewCell.h"
 #import <objc/message.h>
 
+@interface ForwardingTarget : NSObject
+- (void)add;
+@end
+@implementation ForwardingTarget
+- (void)add {
+    NSLog(@"add方法转发给ForwardingTarget执行");
+}
+@end
+
 @interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -56,17 +65,61 @@
 
 #pragma mark - Event Handle
 
+
+#pragma mark - 消息转发
+
+// 默认方法都有两个隐式参数，
 void add(id self, SEL _cmd) {
-    NSLog(@"add ");
+    NSLog(@"动态添加了add方法");
 }
-// 作用:去解决没有实现方法,动态添加方法
-+(BOOL)resolveInstanceMethod:(SEL)sel {
+//  第一步，消息接收者没有找到对应的方法时候，会先调用此方法，可在此方法实现中动态添加新的方法
+//  返回YES表示相应selector的实现已经被找到，或者添加新方法到了类中，否则返回NO
++ (BOOL)resolveInstanceMethod:(SEL)sel {
     if (sel == @selector(add)) {
-        // 添加方法
-        class_addMethod(self, sel, (IMP)add,"v@");
+        // 动态添加方法
+            class_addMethod(self, sel, (IMP)add,"v@:");
         return YES;
     }
     return [super resolveInstanceMethod:sel];
+}
+
+//  第二步， 如果第一步的返回NO或者直接返回了YES而没有添加方法，该方法被调用
+//  在这个方法中，我们可以指定一个可以返回一个可以响应该方法的对象， 注意如果返回self就会死循环
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+//    if (aSelector == @selector(add)) {
+//        //消息转发给ForwardingTarget对象
+//        if ([[ForwardingTarget new] respondsToSelector:@selector(add)]) {
+//           return [ForwardingTarget new];
+//        }
+//    }
+    return [super forwardingTargetForSelector:aSelector];
+}
+
+//  第三步， 如果forwardingTargetForSelector:返回了nil，则该方法会被调用，系统会询问我们要一个合法的『类型编码(Type Encoding)』
+//  若返回 nil，则不会进入下一步，而是无法处理消息
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+//    if (aSelector == @selector(add)) {
+//        return [NSMethodSignature signatureWithObjCTypes:"v@:"];
+//    }
+   return  [super methodSignatureForSelector:aSelector];
+}
+
+// 在这里进行消息转发
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+//    SEL sel = [anInvocation selector];
+//    ForwardingTarget* forward = [ForwardingTarget new];
+//    if ([forward respondsToSelector:sel]) {
+//         //指定消息的接收者
+//         [anInvocation invokeWithTarget:forward];
+//    }else{
+        [super forwardInvocation:anInvocation];
+//    }
+    
+}
+
+// 如果没有实现消息转发 forwardInvocation  则调用此方法
+- (void)doesNotRecognizeSelector:(SEL)aSelector {
+    NSLog(@"找不到方法：%@", NSStringFromSelector(aSelector));
 }
 
 #pragma mark - UITableViewDelegate  UITableViewDataSource
