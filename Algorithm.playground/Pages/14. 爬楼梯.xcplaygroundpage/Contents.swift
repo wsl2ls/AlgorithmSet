05b51d8b9a60bc0fc7f6c374b7525acb2e3becc8
wsl <15324956576@163.com>
2020-03-13 15:32:08 +0800

    14. 爬楼梯

--- Algorithm.playground/Pages/14. 爬楼梯.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/14. 爬楼梯.xcplaygroundpage/Contents.swift
@@ -25,9 +25,8 @@
  
  */
 
-
 /*
- 思路一 递归法
+ 思路1 递归法
  时间复杂度O(2^n)  空间复杂度S(n) 递归的深度可以达到n
  */
 func climbStairs1(_ n: Int) -> Int {
@@ -69,15 +68,55 @@ func climbStairs2(_ i: Int , _ n: Int, _ cache: inout [Int]) -> Int {
 }
 
 /*
- 思路3  记忆化递归 缓存计算过的内容
- 时间复杂度O() 空间复杂度S()
+ 思路3  动态规划
+ 第 i 阶可以由以下两种方法得到：
+ 1. 在第(i−1)阶后向上爬1阶
+ 2. 在第(i−2) 阶后向上爬 2 阶
+ 所以到达第i阶的方法总数就是到第(i-1)阶和第(i-2)阶的方法数之和。
+ 那么dp[i] 表示能到达第i阶的放大总数：dp[i] = dp[i-1] + dp[i-2]
+ 时间复杂度O(n) 空间复杂度S(n)
  */
 func climbStairs3(_ n: Int) -> Int {
-    return 0
+    if n == 1 {
+        return 1
+    }
+    var dp = Array(repeating: 0, count: n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in (3...n) {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    return dp[n]
+}
+
+/*
+ 思路4 斐波那契数
+ 时间复杂度O(n) 空间复杂度S(1)
+ */
+func climbStairs4(_ n: Int) -> Int {
+    if n == 1 {
+        return 1
+    }
+    var first = 1
+    var second = 2
+    for _ in (3...n) {
+        let third = first + second
+        first = second
+        second = third
+    }
+    return second
+}
+
+/*
+ 思路5 斐波那契公式  (示意图Sources目录)
+ 时间复杂度O(log(n)) 空间复杂度S(1)
+ */
+func climbStairs5(_ n: Int) -> Int {
+    return 0;
 }
 
 //test
-var n = 3
-print(climbStairs3(n))
+var n = 4
+print(climbStairs4(n))
 
 
