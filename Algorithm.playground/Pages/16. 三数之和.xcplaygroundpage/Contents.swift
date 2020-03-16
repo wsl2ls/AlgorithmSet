44e56ae5bdac25855e9feb97f91aac9c80f8a36f
wsl <15324956576@163.com>
2020-03-16 19:09:03 +0800

    16. 三数之和

--- Algorithm.playground/Pages/16. 三数之和.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/16. 三数之和.xcplaygroundpage/Contents.swift
@@ -1,15 +1,15 @@
 /* 16. 三数之和
  给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
  注意：答案中不可以包含重复的三元组。
-
+ 
  示例：
  给定数组 nums = [-1, 0, 1, 2, -1, -4]，
  满足要求的三元组集合为：
  [
-   [-1, 0, 1],
-   [-1, -1, 2]
+ [-1, 0, 1],
+ [-1, -1, 2]
  ]
-
+ 
  来源：力扣（LeetCode）
  链接：https://leetcode-cn.com/problems/3sum
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -43,14 +43,16 @@ func threeSum1(_ nums: [Int]) -> [[Int]] {
 func threeSum2(_ nums: [Int]) -> [[Int]] {
     var result: [[Int]] = []
     //O(nlog(n))
-    nums.sorted()
-    
-    
-    return []
+    var sortNums = nums.sorted()
+    print(sortNums)
+    for i in sortNums {
+        
+    }
+    return result
 }
 
 
 //test
 var nums = [-1, 0, 1, 2, -1, -4]
-print(threeSum1(nums))
+print(threeSum2(nums))
 
