732f858b2eeec94955aa4e72bcbb9bf38c594d04
wsl <15324956576@163.com>
2020-03-30 16:20:02 +0800

    22. 柱状图中最大的矩形

--- Algorithm.playground/Pages/22. 柱状图中最大的矩形.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/22. 柱状图中最大的矩形.xcplaygroundpage/Contents.swift
@@ -1,7 +1,6 @@
 /*
  22. 柱状图中最大的矩形
  给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
-
  求在该柱状图中，能够勾勒出来的矩形的最大面积。
 
  [Sources/柱状图中最大的矩形1]
@@ -18,3 +17,33 @@
  链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
+
+/*
+ 思路1：
+ 时间复杂度O(n) 空间复杂度S(1)
+ */
+func largestRectangleArea(_ heights: [Int]) -> Int {
+    if heights.count == 1 {
+        return heights.first!
+    }
+    var maxArea = 0
+    var left = 0
+    var right = heights.count - 1
+    while left < right {
+        maxArea = max(maxArea, (right-left+1)*min(heights[left], heights[right]))
+        print(maxArea)
+        //移动最低边的柱子
+        if heights[left] < heights[right] {
+            left+=1
+        }else {
+            right-=1
+        }
+    }
+    print(maxArea)
+    return maxArea
+}
+
+// test
+var heighs =  [2,1,5,6,2,3]
+largestRectangleArea(heighs)
