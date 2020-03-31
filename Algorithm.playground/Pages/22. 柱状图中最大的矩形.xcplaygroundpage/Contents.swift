51414fa7627aad4a8e79944d2665240243a59495
wsl <15324956576@163.com>
2020-03-31 18:29:12 +0800

    22. 柱状图中最大的矩形

--- Algorithm.playground/Pages/22. 柱状图中最大的矩形.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/22. 柱状图中最大的矩形.xcplaygroundpage/Contents.swift
@@ -2,17 +2,17 @@
  22. 柱状图中最大的矩形
  给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
  求在该柱状图中，能够勾勒出来的矩形的最大面积。
-
+ 
  [Sources/柱状图中最大的矩形1]
  以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
-
+ 
  [Sources/柱状图中最大的矩形2]
  图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
-
+ 
  示例:
  输入: [2,1,5,6,2,3]
  输出: 10
-
+ 
  来源：力扣（LeetCode）
  链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -29,7 +29,7 @@ func largestRectangleArea1(_ heights: [Int]) -> Int {
         for j in (i..<heights.count) {
             var minHieght = Int.max
             for k in (i...j) {
-                 minHieght = min(minHieght, heights[k])
+                minHieght = min(minHieght, heights[k])
             }
             maxArea = max(maxArea, (j-i+1)*minHieght)
         }
@@ -57,12 +57,13 @@ func largestRectangleArea2(_ heights: [Int]) -> Int {
 }
 
 /*
- 思路3：栈
+ 思路3：单调递增栈
  时间复杂度O(n) 空间复杂度S(n)
  */
 func largestRectangleArea3(_ heights: [Int]) -> Int {
     var stack: [Int] = []
     var maxArea: Int = 0
+    //为了减少临界处理，我们在 heights 数组的左右两端都塞入0，也就是加入了两根高度为0的柱子。
     let heights = [0] + heights + [0]
     
     for i in 0..<heights.count {
@@ -72,11 +73,14 @@ func largestRectangleArea3(_ heights: [Int]) -> Int {
         }
         stack.append(i)
     }
+    
     print(maxArea)
     return maxArea
 }
 
 
 // test
-var heighs =  [2,1,5,6,2,3]
+//var heighs =  [2,1,5,6,2,3]
+var heighs =  [2, 1, 2]
+//var heighs =  [1, 1, 1]
 largestRectangleArea3(heighs)
