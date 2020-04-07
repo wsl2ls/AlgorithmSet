bcf2a247644eb925d02b84ade8762b445a80fd58
wsl <15324956576@163.com>
2020-04-07 20:04:01 +0800

    26. 整数反转

--- Algorithm.playground/Pages/26. 整数反转.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/26. 整数反转.xcplaygroundpage/Contents.swift
@@ -14,10 +14,27 @@
  输出: 21
  
  注意:
- 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
 
  来源：力扣（LeetCode）
  链接：https://leetcode-cn.com/problems/reverse-integer
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
+
+
+func reverse(_ x: Int) -> Int {
+    var m = x
+    var n = 0
+    while m != 0 {
+        n  = n*10 + m%10
+        m = m/10
+    }
+    if n > Int32.max || n < Int32.min {
+        return 0;
+    }
+    return n
+}
+
+print(reverse(-123456))
 
