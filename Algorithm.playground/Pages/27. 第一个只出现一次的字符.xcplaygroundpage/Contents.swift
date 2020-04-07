66a7c9b0027da8a676533bae831fa9bbe1113649
wsl <15324956576@163.com>
2020-04-07 20:39:17 +0800

    第一个只出现一次的字符

---
+++ Algorithm.playground/Pages/27. 第一个只出现一次的字符.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+/* 27. 第一个只出现一次的字符
+ 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。
+
+ 示例:
+
+ s = "abaccdeff"
+ 返回 "b"
+
+ s = ""
+ 返回 " "
+  
+
+ 限制：
+ 0 <= s 的长度 <= 50000
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+func firstUniqChar(_ s: String) -> Character {
+    var char = Character.init("")
+    
+    
+    
+    
+    return char
+}
+
+
+print(firstUniqChar("abaccdeff"))
